a7982396fb26b85d682b6b008c4b51d7bcdeb0d5
Nikolay Dimitrov <n.zhdimitrov@gmail.com>
2024-12-09 03:47:47 +0200

    Adjusted colors.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -33,7 +33,7 @@ export const Login: React.FC = () => {
     return (
         <div className="flex items-center justify-center h-screen font-Josefin">
             <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
-                <h2 className="text-2xl font-bold mb-4">Welcome to Indumenta</h2>
+                <h2 className="text-2xl font-bold mb-4 text-secondary">Welcome to Indumenta</h2>
                 {error && <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">{error}</div>}
                 <div className="mb-4">
                     <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
